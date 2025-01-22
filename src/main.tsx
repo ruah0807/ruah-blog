f43165d40effb73beaa77c867cbc0b6e49ecc4d8
Ruah Kim <kimhk0315@gmail.com>
2025-01-23 00:22:06 +0900

    feat : change main.tsx for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/global.css'; 
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import{ RouterProvider, createHashRouter} from "react-router-dom"
 
+const router = createHashRouter([
+  {
+    path: "/ruah-blog",
+    element: <App />,
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
