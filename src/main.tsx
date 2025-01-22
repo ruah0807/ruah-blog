17b675b4b4323258cba61147fbd23cb9ba4d57d7
Ruah Kim <kimhk0315@gmail.com>
2025-01-23 02:04:46 +0900

    fix : router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 import App from './App.tsx'
 import './styles/global.css'; // global.css를 나중에 임포트
 // import './App.css';
 
-import { Buffer } from 'buffer';
+// import { Buffer } from 'buffer';
 
-// Example usage
-const buf = Buffer.from('Hello, world!', 'utf-8');
-console.log(buf.toString('hex'));
+// // Example usage
+// const buf = Buffer.from('Hello, world!', 'utf-8');
+// console.log(buf.toString('hex'));
 
-ReactDOM.render(
+const router = createHashRouter([
+  {
+    path: "/ruah-blog/",
+    element: <App />,
+    loader: () => <div>로딩중...</div>,
+    ErrorBoundary: () => <div>404 NOT FOUND</div>,
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <RouterProvider router={router} />
+  </React.StrictMode>
 );
