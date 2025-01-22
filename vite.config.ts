875cc8851ba21be67c41cd1249cf7b1342972bfd
Ruah Kim <kimhk0315@gmail.com>
2025-01-22 16:53:10 +0900

    fix : vite.config.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
+  base: '/ruah-blog/',
   plugins: [react()],
   assetsInclude: ['**/*.md'],
   optimizeDeps: {
