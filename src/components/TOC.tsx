d43fd08dd6873e638edea89c77ff6f39deef54da
Ruah Kim <kimhk0315@gmail.com>
2025-01-24 01:32:59 +0900

    fix : TOC

--- src/components/TOC.tsx
+++ src/components/TOC.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import matter from 'gray-matter';
 import './TOC.css';
 
 interface TOCProps {
@@ -9,20 +10,32 @@ const TOC: React.FC<TOCProps> = ({ fileName }) => {
   const [headings, setHeadings] = useState<{ text: string; id: string; level: number }[]>([]);
 
   useEffect(() => {
-    const extractHeadings = () => {
-      const contentElement = document.querySelector('.markdown-content');
-      if (contentElement) {
-        const headingElements = contentElement.querySelectorAll('h1, h2, h3, h4, h5');
-        const extractedHeadings = Array.from(headingElements).map(heading => ({
-          text: heading.textContent || '',
-          id: heading.id,
-          level: parseInt(heading.tagName.substring(1), 10),
-        }));
-        setHeadings(extractedHeadings);
+    const fetchContent = async () => {
+      if (fileName) {
+        const fileModules = import.meta.glob('/posts/**/*.md', { query: '?raw', import: 'default' });
+        const filePath = `/posts/${fileName}.md`;
+
+        if (fileModules[filePath]) {
+          const content = await fileModules[filePath]();
+          const { content: mdContent } = matter(content as string);
+
+          const headingRegex = /^(?!#\s)(#{1,5})\s+(.*)$/gm;
+          const matches = [...mdContent.matchAll(headingRegex)];
+
+          const extractedHeadings = matches.map(match => ({
+            text: match[2],
+            id: match[2].toLowerCase().replace(/\s+/g, '-'),
+            level: match[1].length,
+          }));
+
+          setHeadings(extractedHeadings);
+        } else {
+          console.error(`File not found: ${filePath}`);
+        }
       }
     };
 
-    extractHeadings();
+    fetchContent();
   }, [fileName]);
 
   return (
