c7796458082403a32d5d05add749f1d0ccc484ee
Ruah Kim <kimhk0315@gmail.com>
2025-01-23 12:11:34 +0900

    fix : 상대경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,8 @@ const App: React.FC = () => {
   }
 
   return (
-    <Router  basename={`${process.env.PUBLIC_URL}`}>
-    {/* // <Router  basename='/ruah-blog/'>  */}
+    // <Router  basename={`${process.env.PUBLIC_URL}`}>
+    <Router  basename='/ruah-blog/'>  
     {/* <Router> */}
       <Header toggleSidebar={toggleSidebar} />
       <div className={`container ${showSidebar ? 'show-sidebar' : ''}`}>
